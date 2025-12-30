bcfc1bf65f24faf4ac832423635eb72e058b8ef4
김민지 <alswlk0306@gmail.com>
2025-12-31 02:50:23 +0900

    design: tailwind custom color 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,8 +4,32 @@ export default {
   theme: {
     extend: {
       colors: {
-        primary: "#123123",
-      }, // tailwindcss custom test를 위한 코드
+        // Brand / main colors
+        brand: {
+          primary: "#2B7FFF", // main blue
+          primarySoft: "#2B7FFF26", // main blue (15% opacity)
+          green: "#33C894", // success
+          yellow: "#FAE164", // warning
+          orange: "#F17148", // accent
+        },
+
+        // Semantic colors
+        error: {
+          DEFAULT: "#FF3939",
+        },
+
+        // Gray (neutral)
+        gray: {
+          50: "#F7F7F8",
+          100: "#E1E2E4",
+          200: "#AEB0B6",
+          600: "#5A5C63",
+          900: "#292A2D",
+          950: "#171719",
+        },
+      },
+
+      // Font system
       fontFamily: {
         kr: ["Pretendard", "Apple SD Gothic Neo", "Noto Sans KR", "system-ui", "sans-serif"],
         en: ["Roboto", "system-ui", "sans-serif"],
