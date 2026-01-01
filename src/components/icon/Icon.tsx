76e70df2016d4d6a4c58c955de435c79fa164d20
김민지 <alswlk0306@gmail.com>
2026-01-02 03:13:24 +0900

    design: 아이콘 컴포넌트화

---
+++ src/components/icon/Icon.tsx
@@ -0,0 +1,11 @@
+interface IconProps {
+  name: string; // svg 파일명 (확장자 제외)
+  className?: string; // tailwind 스타일
+}
+
+// 공통 Icon 컴포넌트
+const Icon = ({ name, className }: IconProps) => {
+  return <img src={`/src/assets/icons/${name}.svg`} alt={name} className={className} />;
+};
+
+export default Icon;
