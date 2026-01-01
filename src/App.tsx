beb8013c58a23a5b50e230a9caec2a4e6b5bc957
김민지 <alswlk0306@gmail.com>
2026-01-02 03:14:49 +0900

    feat: 라우트별 레이아웃(HeaderOnly / Default) 분리

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,24 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import ColorGuide from "./pages/guide/ColorGuide";
+import HeaderOnlyLayout from "./layouts/HeaderOnlyLayout";
+import DefaultLayout from "./layouts/DefaultLayout";
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
+        {/* 헤더만 있는 레이아웃 (온보딩/로그인/회원가입) */}
+        <Route path="/" element={<HeaderOnlyLayout />}>
+          {/* <Route index element={<OnboardingPage />} /> */}
+        </Route>
+
+        {/* 기본 레이아웃 (헤더+푸터) */}
+        <Route element={<DefaultLayout />}>
+          {/* <Route path="/home" element={<HomePage />} /> */}
+          {/* <Route path="/symptom" element={<SymptomPage />} /> */}
+          {/* <Route path="/my" element={<MyPage />} /> */}
+        </Route>
+
         {/* tailwind custom color 시각화 */}
         <Route path="/color-guide" element={<ColorGuide />} />
 
