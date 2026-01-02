3a1a683f53068d0ac0f5190f4498ac21a01a67c5
김민지 <alswlk0306@gmail.com>
2026-01-02 22:39:34 +0900

    fix: HeaderOnlyLayout이랑 같은 원칙으로 DefaultLayout도 flex 레이아웃으로 변경

--- src/layouts/DefaultLayout.tsx
+++ src/layouts/DefaultLayout.tsx
@@ -4,14 +4,17 @@ import { Outlet } from "react-router-dom";
 
 const DefaultLayout = () => {
   return (
-    <>
+    <div className="flex min-h-screen flex-col">
       {/* 기본 페이지: Header + Footer */}
       <Header />
-      <main className="min-h-[calc(100vh-64px)]">
+
+      {/* Header/Footer 사이 공간 */}
+      <main className="flex-1">
         <Outlet />
       </main>
+
       <Footer />
-    </>
+    </div>
   );
 };
 
