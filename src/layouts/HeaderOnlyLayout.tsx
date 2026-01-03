4184ee9608d551dbb3406b21cdab5f8901c15dcd
김민지 <alswlk0306@gmail.com>
2026-01-04 02:29:38 +0900

    refactor: 폴더 구조 변경으로 인한 import 경로 변경

--- src/layouts/HeaderOnlyLayout.tsx
+++ src/layouts/HeaderOnlyLayout.tsx
@@ -1,4 +1,4 @@
-import Header from "@/components/common/header/Header";
+import Header from "@/components/header/Header";
 import { Outlet } from "react-router-dom";
 
 const HeaderOnlyLayout = () => {
