491b8c9ece9ea0b8c7981128f80c2e1d46a71e06
김민지 <alswlk0306@gmail.com>
2026-01-11 23:21:48 +0900

    style: 멘트 수정

--- src/pages/signup/SignUpSymptomPage.tsx
+++ src/pages/signup/SignUpSymptomPage.tsx
@@ -22,19 +22,20 @@ const SignUpSymptomPage = () => {
         <button
           type="button"
           onClick={handleBack}
-          className="absolute left-[48px] top-[99px] h-[45px] w-[37px] gap-[10px] p-[10px]"
+          className="absolute left-[48px] top-[99px] h-[105px] w-[37px] gap-[10px] p-[10px]"
           aria-label="뒤로가기"
         >
           <Icon name="arrow-back" className="h-full w-full" />
         </button>
 
-        <div className="flex h-[80px] flex-col items-center gap-[5px]">
+        <div className="flex h-[80px] flex-col items-center gap-6">
           <h1 className="flex h-[50px] items-center justify-center px-[10px] text-center text-[36px] font-extrabold leading-[1.18] tracking-[-0.025em] text-brand-primary">
             주로 아픈 곳을 선택해주세요!
           </h1>
 
           <p className="flex h-[25px] items-center justify-center px-[10px] text-center text-[18px] font-medium leading-[1.18] tracking-[-0.025em] text-gray-950">
-            선택한 부위를 기반으로 대처 가이드와 병원 정보를 추천해드립니다
+            선택한 부위를 기반으로 대처 가이드와 병원 정보를 추천해드립니다 <br />
+            필요하다면 증상을 선택하지 않고 넘어갈 수도 있어요
           </p>
         </div>
 
