13e31e6fb175c81a817df3355f719c250cfcbcd9
김민지 <alswlk0306@gmail.com>
2026-01-25 01:49:47 +0900

    refactor: 로그인 및 회원가입 좌우 여백 수정

--- src/pages/signup/SignUpPage.tsx
+++ src/pages/signup/SignUpPage.tsx
@@ -5,7 +5,7 @@ const SignUpPage = () => {
   return (
     <div className="flex flex-col bg-white pt-9">
       <AuthHeader backTo="/select-symptom" />
-      <div className="flex justify-center pt-[0px]">
+      <div className="flex justify-center pt-[0px] px-6">
         <div className="flex w-full max-w-[354px] flex-col items-center md:max-w-[404px]">
           <div className="h-[40px]" />
           <div className="flex w-full flex-col items-center gap-[10px] pb-[40px]">
