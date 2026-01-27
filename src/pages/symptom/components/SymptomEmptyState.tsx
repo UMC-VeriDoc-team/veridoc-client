270d176f5856808ef99987b9ab77fd84e424baf5
정윤주 <oyunjooya@naver.com>
2026-01-27 23:09:23 +0900

    design: 생활 관리 페이지 반응형 UI 수정

--- src/pages/symptom/components/SymptomEmptyState.tsx
+++ src/pages/symptom/components/SymptomEmptyState.tsx
@@ -8,14 +8,34 @@ interface SymptomEmptyStateProps {
 
 export const SymptomEmptyState = ({ onClickSelectSymptom }: SymptomEmptyStateProps) => {
   return (
-    <div className="flex flex-col items-center">
+    <div className="flex w-full flex-col md:items-center">
       <SectionTitle
-        title="아직 선택한 증상이 없어요"
+        title={
+          <>
+            {/* 모바일 */}
+            <span className="block w-full whitespace-pre-line md:hidden">
+              아직 선택한{"\n"}
+              증상이 없어요
+            </span>
+
+            {/* 데스크탑 */}
+            <span className="hidden text-center md:block">아직 선택한 증상이 없어요</span>
+          </>
+        }
         description={
           <>
-            증상을 선택하면 해당 증상에 대한 정보와 일반적인 대응 흐름을 확인할 수 있어요
-            <br />
-            선택한 증상은 언제든 변경할 수 있어요
+            {/* 모바일 */}
+            <span className="block w-full whitespace-pre-line md:hidden">
+              증상을 선택하면 해당 증상에 대한 정보와 일반적인 대응{"\n"}
+              흐름을 확인할 수 있어요 선택한 증상은 언제든 변경할 수 {"\n"}있어요
+            </span>
+
+            {/* 데스크탑 */}
+            <span className="hidden md:block">
+              증상을 선택하면 해당 증상에 대한 정보와 일반적인 대응 흐름을 확인할 수 있어요
+              <br />
+              선택한 증상은 언제든 변경할 수 있어요
+            </span>
           </>
         }
       />
@@ -23,11 +43,16 @@ export const SymptomEmptyState = ({ onClickSelectSymptom }: SymptomEmptyStatePro
       <img
         src={symptomNotSelectedImg}
         alt="증상 미선택"
-        className="mt-[54px] h-auto w-full max-w-[603px]"
         draggable={false}
+        className={[
+          // 모바일
+          "mt-8 h-[342px] w-[380px] object-contain",
+          // 데스크탑
+          "md:mt-[54px] md:h-auto md:w-full md:max-w-[603px]",
+        ].join(" ")}
       />
 
-      <div className="mt-16 w-[404px]">
+      <div className="mt-10 w-[354px] md:mt-16 md:w-[404px]">
         <Button type="button" onClick={onClickSelectSymptom}>
           증상 선택하기
         </Button>
