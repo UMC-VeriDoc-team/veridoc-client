1486f91f67b39436846b6ec9cf30bb8351a6496a
김민지 <alswlk0306@gmail.com>
2026-01-29 01:18:17 +0900

    design: 모바일 tab bar 구현

--- src/layouts/DefaultLayout.tsx
+++ src/layouts/DefaultLayout.tsx
@@ -1,10 +1,14 @@
+import BottomTabBar from "@/components/BottomTabBar/BottomTabBar";
 import Footer from "@/components/Footer/Footer";
 import Header from "@/components/Header/Header";
+import useIsMobile from "@/hooks/useIsMobile";
 import { Outlet } from "react-router-dom";
 
 const DefaultLayout = () => {
+  const isMobile = useIsMobile();
+
   return (
-    <div className="flex min-h-screen flex-col">
+    <div className={`flex min-h-screen flex-col ${isMobile && "pb-16"}`}>
       {/* 기본 페이지: Header + Footer */}
       <Header className="sticky top-0 z-50" />
 
@@ -13,7 +17,7 @@ const DefaultLayout = () => {
         <Outlet />
       </main>
 
-      <Footer />
+      {isMobile ? <BottomTabBar /> : <Footer />}
     </div>
   );
 };
