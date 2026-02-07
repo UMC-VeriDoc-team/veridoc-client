ba1a5819648b8de48130878496d4a5ea1be80d67
김민지 <alswlk0306@gmail.com>
2026-02-08 05:02:45 +0900

    fix: API 호출 오류ㅠ 수정

--- src/stores/login/useAuthStore.ts
+++ src/stores/login/useAuthStore.ts
@@ -14,8 +14,6 @@ type LoginErrorBody = { code?: string };
 interface AuthState {
   // auth
   accessToken: string | null;
-  userID: number | null;
-  painAreaID: number | null;
 
   isLoggedIn: boolean;
   loading: boolean;
