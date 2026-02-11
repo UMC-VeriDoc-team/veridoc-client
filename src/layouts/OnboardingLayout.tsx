1ec9f2f624257244b5a9c5ff7bd0ec4572aac5f9
김민지 <alswlk0306@gmail.com>
2026-02-11 18:55:52 +0900

    feat: 모바일 온보딩 하단 메뉴바 추가

--- src/layouts/OnboardingLayout.tsx
+++ src/layouts/OnboardingLayout.tsx
@@ -1,12 +1,17 @@
+import BottomTabBar from "@/components/BottomTabBar/BottomTabBar";
 import Header from "@/components/Header/Header";
+import useIsMobile from "@/hooks/useIsMobile";
 import { Outlet } from "react-router-dom";
 
 const OnboardingLayout = () => {
+  const isMobile = useIsMobile();
+
   return (
     <div className="flex h-dvh flex-col overflow-hidden">
       <Header className="sticky top-0 z-50" />
       <main className="flex-1 overflow-hidden">
         <Outlet />
+        {isMobile && <BottomTabBar />}
       </main>
     </div>
   );
